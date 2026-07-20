1dc615c197a912797471bcf8c86283c666c96dfd
gtripletsyt <108158973+gtripletsyt@users.noreply.github.com>
2026-07-20 13:16:03 -0500

    Update updates.js
    missing comma

--- functions/api/updates.js
+++ functions/api/updates.js
@@ -3,13 +3,14 @@ export async function onRequest(context) {
   const updates = [
     {
       title: "less work on the site",
-      description: "hello everyone. so this website will most likely not have a lot of work done on it, since i grinded to get the main part done. i might update this every once in a while to tell yall how im doing, but since there's nothing to add to the site as much, i wont do as much. i might edit some stuff every day but not much. thanks for reading!"
+      description: "hello everyone. so this website will most likely not have a lot of work done on it, since i grinded to get the main part done. i might update this every once in a while to tell yall how im doing, but since there's nothing to add to the site as much, i wont do as much. i might edit some stuff every day but not much. thanks for reading!",
       author: "gtripletsyt",
       date: "2026-07-20T18:15:43Z",,
       url: "#"
     },
     {
       title: "testing",
+      description: "hopefully this works otherwise im done for",
       author: "gtripletsyt",
       date: "2026-07-19T15:15:02Z",
       url: "#"
